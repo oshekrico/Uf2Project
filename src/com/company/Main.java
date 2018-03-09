6b755ded102680c548999fbeb0b3d80a51e7621b
nayan0048 <nayan0048@gmail.com>
2018-03-09 18:52:55 +0100

    PUTODANI.txt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("el nayan la chupa");
+        System.out.println("PUTODANI!!");
     }
 
 }
