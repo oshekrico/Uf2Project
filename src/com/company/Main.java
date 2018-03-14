c6782405578dd2a738cd1cf6b6e1f3c39d9c5cf3
manchita360 <jona.manchis@gmail.com>
2018-03-14 16:45:12 +0100

    null commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	//22
-        System.out.println("el nayan la chupa");
-        System.out.println("PUTODANI!!");
-        System.out.println("EL LAGUA TB ");
+
     }
 
 }
