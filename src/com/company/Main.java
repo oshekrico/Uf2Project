ddf52810a5da6c30a58a70eaba7ae41566de7512
nayan0048 <nayan0048@gmail.com>
2018-03-14 16:31:30 +0100

    PUTODANI.txt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// El nayan la chupa pero en comentario
+	//
         System.out.println("el nayan la chupa");
         System.out.println("PUTODANI!!");
         System.out.println("EL LAGUA TB ");
