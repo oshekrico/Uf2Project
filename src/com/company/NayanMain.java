381f706e6aa558110cfb7a1b3e678eb15c5a82c1
nayan0048 <nayan0048@gmail.com>
2018-03-16 19:10:10 +0100

    GG.txt

--- src/com/company/NayanMain.java
+++ src/com/company/NayanMain.java
@@ -4,25 +4,51 @@ import com.company.model.Anime;
 
 public class NayanMain {
     public static void main(String[] args) {
+
         Anime a1 = new Anime();
-        a1.nombreAnime = "Fairy tail";
-        a1.sinopsis = "Anime magia";
-        a1.numeroDeCapitulos = 277;
-        a1.fechaDeLanzamiento = 2009;
+        //  a1.nombreAnime = "Fairy tail";
+        //   a1.sinopsis = "Anime magia";
+        //  a1.numeroDeCapitulos = 277;
+        //   a1.fechaDeLanzamiento = "12 de  octubre de 2009";
 
 
         Anime a2 = new Anime();
-        a2.nombreAnime = "Naruto";
-        a2.sinopsis = "Anime Lucha";
-        a2.numeroDeCapitulos = 386;
-        a2.fechaDeLanzamiento = 2002;
+     //   a2.nombreAnime = "Slam Dunk";
+      //  a2.sinopsis = "Anime Deporte";
+      //  a2.numeroDeCapitulos = 101;
+      //  a2.fechaDeLanzamiento = 16 de octubre de 1993;
 
 
         Anime a3 = new Anime();
-        a3.nombreAnime = "One piece";
-        a3.sinopsis = "Anime Aventura";
-        a3.numeroDeCapitulos = 800;
-        a3.fechaDeLanzamiento = 1997;
+     //   a3.nombreAnime = "One piece";
+     //   a3.sinopsis = "Anime Aventura";
+     //   a3.numeroDeCapitulos = 827;
+    //    a3.fechaDeLanzamiento = 20 de octubre de 1999;
+
+        Anime a4 = new Anime();
+        //   a3.nombreAnime = "Bleach";
+        //   a3.sinopsis = "Anime Shonen";
+        //   a3.numeroDeCapitulos = 800;
+        //    a3.fechaDeLanzamiento = 5 de octubre de 2004;
+
+        Anime a5 = new Anime();
+        //   a3.nombreAnime = "Pingu";
+        //   a3.sinopsis = "Anime Comedia";
+        //   a3.numeroDeCapitulos = 157;
+        //    a3.fechaDeLanzamiento = 7 de marzo de 1990;
+
+        a1.asignarNombreAnime("Fairy tail");
+        a1.asignarsinopsis("Anime Aventura");
+        a1.asignarfechaDeLanzamiento();
+        a1.asignarnumeroDeCapitulos();
+
+        a2.asignarNombreAnime("Slam Dunk");
+        a3.asignarNombreAnime("One piece");
+        a4.asignarNombreAnime("Bleach");
+        a5.asignarNombreAnime("Pingu");
+
+
+
     }
 
 }
--- src/com/company/model/Anime.java
+++ src/com/company/model/Anime.java
@@ -3,9 +3,27 @@ package com.company.model;
 public class Anime {
     //campo
 
- public String nombreAnime;
- public  String sinopsis;
- public int fechaDeLanzamiento;
- public int numeroDeCapitulos;
+    public String nombreAnime;
+    public  String sinopsis;
+    public String fechaDeLanzamiento;
+    public int numeroDeCapitulos;
+
+    //metodos
+
+    public void asignarNombreAnime ( String nombre) {
+          nombreAnime= nombre;
+    }
+
+    public void asignarsinopsis ( String sinopsisanime) {
+        sinopsis= sinopsisanime;
+    }
+
+    public void asignarfechaDeLanzamiento ( String fechaDeLanzamientoAnime) {
+        fechaDeLanzamiento= fechaDeLanzamientoAnime;
+    }
+
+    public void asignarnumeroDeCapitulos ( int numeroDeCapitulosAnime) {
+        numeroDeCapitulos= numeroDeCapitulosAnime;
+    }
 
 }
