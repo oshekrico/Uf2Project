db0bf0e20dff3ac5148ff6531a88d6cb1e63a9f2
nayan0048 <nayan0048@gmail.com>
2018-03-16 18:32:35 +0100

    GG.txt

--- src/com/company/NayanMain.java
+++ src/com/company/NayanMain.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import com.company.model.Anime;
+
 public class NayanMain {
     public static void main(String[] args) {
+        Anime a1 = new Anime();
+        a1.nombreAnime = "Fairy tail";
+        a1.sinopsis = "Anime magia";
+        a1.numeroDeCapitulos = 277;
+        a1.fechaDeLanzamiento = 2009;
 
+
+        Anime a2 = new Anime();
+        a2.nombreAnime = "Naruto";
+        a2.sinopsis = "Anime Lucha";
+        a2.numeroDeCapitulos = 386;
+        a2.fechaDeLanzamiento = 2002;
+
+
+        Anime a3 = new Anime();
+        a3.nombreAnime = "One piece";
+        a3.sinopsis = "Anime Aventura";
+        a3.numeroDeCapitulos = 800;
+        a3.fechaDeLanzamiento = 1997;
     }
 
 }
---
+++ src/com/company/model/Anime.java
@@ -0,0 +1,11 @@
+package com.company.model;
+
+public class Anime {
+    //campo
+
+ public String nombreAnime;
+ public  String sinopsis;
+ public int fechaDeLanzamiento;
+ public int numeroDeCapitulos;
+
+}
