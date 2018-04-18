0d80e09ade9344176b4313311bf40e0d90aa8ea6
nayan0048 <nayan0048@gmail.com>
2018-04-18 17:03:23 +0200

    GG.txt

--- src/com/company/manager/ManagerAnimes.java
+++ src/com/company/manager/ManagerAnimes.java
@@ -58,12 +58,36 @@ public class ManagerAnimes {
     }
 
 
-    public void crearAnimesIniciales(){
-        crearAnime("HOLA", "ANIME", "tuyjf", "fd/ds7/8", 1, "ADIOS");
-        crearAnime("HOLA2", "ANIME2", "tuyjf", "fd/ds7/8", 1, "ADIOS");
-        crearAnime("HOLA3", "ANIME", "tuyjf", "fd/ds7/8", 1, "ADIOS");
-        crearAnime("HOLA4", "ANIME", "tuyjf", "fd/ds7/8", 1, "ADIOS");
-        crearAnime("HOLA5", "ANIME", "tuyjf", "fd/ds7/8", 1, "ADIOS");
-        crearAnime("HOLA6", "ANIME", "tuyjf", "fd/ds7/8", 1, "ADIOS");
+    public void crearAnimesIniciales() {
+        crearAnime("Fairy Tail", "^.^", "fantasía", "Un día, Lucy Heartfilia, una joven de 17 años de edad especializada en la magia de espíritus celestiales, huye" +
+                " de su hogar y viaja al reino de Fiore para cumplir su sueño de unirse al gremio de Fairy Tail. En el camino conoce a Natsu Dragneel, un joven que busca a Igneel (un dragón que" +
+                " lo crio como un padre adoptivo en su infancia), y que viaja acompañado de su mejor amigo, un gato parlante llamado Happy", "12/10/2009", 277, "Pictures");
+
+        crearAnime("One Piece", ":D", "aventura", "La serie comienza con la ejecución de Gol D. Roger, un hombre conocido como el Rey de los Piratas. Poco \" +\n" +
+                "\"antes de su muerte, Roger hace mención a su gran tesoro legendario, el «One Piece» , y a que puede ser tomado por todo aquél que lo desee. Esto marca el inicio de una" +
+                " era conocida como la Gran Era Pirata. Como resultado, un sinnúmero de piratas zarparon hacia Grand Line con el objetivo de encontrarlo", "20/10/1999", 832, "Toei Animation");
+
+        crearAnime("Naruto",":)"," Acción","Naruto Uzumaki es un ninja adolescente que tiene encerrado en su interior al «Zorro de Nueve Colas»  Doce años" +
+                " antes del inicio de la serie, este demonio atacó a la Aldea Oculta de la Hoja matando a muchas personas. Como consecuencia, el líder de la aldea (el Cuarto Hokage," +
+                " Minato Namikaze) sacrificó su vida utilizando el Shiki Fūjin para sellarlo dentro de Naruto cuando era un recién nacido, con lo que esperaba detener la masacre. Por ello," +
+                " Naruto fue víctima de maltratos por quienes lo consideraban como el propio Kyūbi","15/02/2007",500," Pierrot");
+
+        crearAnime("Dragon Ball","-_-"," Acción","Freezer es un ser que conquista planetas para venderlos después a través de una organizaciónNota a la que pertenecen" +
+                " criaturas de distintas razas. Una de ellas es la de los saiyajin, seres con aspecto humano y poseedores de mucha fuerza. Temeroso de que representen una amenaza para su dominio, Freezer" +
+                " decide usar sus poderes para exterminarlos destruyendo su hogar, el planeta Vegeta.Solo unos cuantos saiyajin sobreviven a la masacre, entre ellos Vegeta, Nappa y los hermanos Raditz y Kakarotto," +
+                " a quienes prefiere dejar con vida para que sigan sirviendo a su propósito","",159,"Toei Animation");
+
+        crearAnime("Bleach","._.","aventura","La serie narra las aventuras de Ichigo Kurosaki, un adolescente que tiene la facultad de interactuar con los espíritus. Una noche, Ichigo se " +
+                "encuentra con una shinigami —personificación japonesa del Dios de la muerte— llamada Rukia Kuchiki, quien se sorprende que pueda verla. Sin embargo, su conversación es interrumpida por la aparición de un" +
+                " hollow, un espíritu maligno. Después de que Rukia fuera gravemente herida al tratar de proteger a Ichigo, esta intenta traspasarle parte de sus poderes a Ichigo para que pueda enfrentarse al hollow en igualdad " +
+                "de condiciones","05/10/2004",366,"Pierrot");
+
+        crearAnime("Shingeki no Kyojin","q.q","fantasía","Hace más de 100 años, la humanidad es casi aniquilada por unas siniestras criaturas desde unos 3 hasta 15 metros de altura,conocidas como los " +
+                "Titanes, que surgen de la nada con un único propósito: devorar seres humanos. No obstante, un grupo reducido de personas pudo sobrevivir dentro de una enorme área de terreno amurallada, protegida por muros de 50 metros" +
+                " de altura, el triple de los titanes más altos registrados hasta la fecha de su construcción","",,"");
+
+        crearAnime("","","","","06/04/2013",30,"Wit Studio");
+
+
     }
 }
