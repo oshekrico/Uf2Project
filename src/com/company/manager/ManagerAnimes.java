0fef0dedf469a6127c907c6889fba18070501bd7
nayan0048 <nayan0048@gmail.com>
2018-04-20 17:34:40 +0200

    GG.txt

--- src/com/company/manager/ManagerAnimes.java
+++ src/com/company/manager/ManagerAnimes.java
@@ -3,7 +3,7 @@ package com.company.manager;
 import com.company.model.Anime;
 
 public class ManagerAnimes {
-    Anime[] animes = new Anime[40];
+    private Anime[] animes = new Anime[40];
 
     public void crearAnime(String titulo, String portada, String genero, String sinopsis, String fechaLanzamiento, int numeroCapitulos, String estudio){
 
@@ -59,13 +59,25 @@ public class ManagerAnimes {
 
 
     public void crearAnimesIniciales() {
+
         crearAnime("Fairy Tail", "^.^", "fantasía", "Un día, Lucy Heartfilia, una joven de 17 años de edad especializada en la magia de espíritus celestiales, huye" +
                 " de su hogar y viaja al reino de Fiore para cumplir su sueño de unirse al gremio de Fairy Tail. En el camino conoce a Natsu Dragneel, un joven que busca a Igneel (un dragón que" +
                 " lo crio como un padre adoptivo en su infancia), y que viaja acompañado de su mejor amigo, un gato parlante llamado Happy", "12/10/2009", 277, "Pictures");
 
+        crearAnime("Shingeki no Kyojin","q.q","fantasía","Hace más de 100 años, la humanidad es casi aniquilada por unas siniestras criaturas desde unos 3 hasta 15 metros de altura,conocidas como los " +
+                "Titanes, que surgen de la nada con un único propósito: devorar seres humanos. No obstante, un grupo reducido de personas pudo sobrevivir dentro de una enorme área de terreno amurallada, protegida por muros de 50 metros" +
+                " de altura, el triple de los titanes más altos registrados hasta la fecha de su construcción","06/04/2013",30,"Wit Studio");
+
+
+        crearAnime("Bleach","._.","aventura","La serie narra las aventuras de Ichigo Kurosaki, un adolescente que tiene la facultad de interactuar con los espíritus. Una noche, Ichigo se " +
+                "encuentra con una shinigami —personificación japonesa del Dios de la muerte— llamada Rukia Kuchiki, quien se sorprende que pueda verla. Sin embargo, su conversación es interrumpida por la aparición de un" +
+                " hollow, un espíritu maligno. Después de que Rukia fuera gravemente herida al tratar de proteger a Ichigo, esta intenta traspasarle parte de sus poderes a Ichigo para que pueda enfrentarse al hollow en igualdad " +
+                "de condiciones","05/10/2004",366,"Pierrot");
+
         crearAnime("One Piece", ":D", "aventura", "La serie comienza con la ejecución de Gol D. Roger, un hombre conocido como el Rey de los Piratas. Poco \" +\n" +
                 "\"antes de su muerte, Roger hace mención a su gran tesoro legendario, el «One Piece» , y a que puede ser tomado por todo aquél que lo desee. Esto marca el inicio de una" +
                 " era conocida como la Gran Era Pirata. Como resultado, un sinnúmero de piratas zarparon hacia Grand Line con el objetivo de encontrarlo", "20/10/1999", 832, "Toei Animation");
+
 
         crearAnime("Naruto",":)"," Acción","Naruto Uzumaki es un ninja adolescente que tiene encerrado en su interior al «Zorro de Nueve Colas»  Doce años" +
                 " antes del inicio de la serie, este demonio atacó a la Aldea Oculta de la Hoja matando a muchas personas. Como consecuencia, el líder de la aldea (el Cuarto Hokage," +
@@ -77,14 +89,37 @@ public class ManagerAnimes {
                 " decide usar sus poderes para exterminarlos destruyendo su hogar, el planeta Vegeta.Solo unos cuantos saiyajin sobreviven a la masacre, entre ellos Vegeta, Nappa y los hermanos Raditz y Kakarotto," +
                 " a quienes prefiere dejar con vida para que sigan sirviendo a su propósito","",159,"Toei Animation");
 
-        crearAnime("Bleach","._.","aventura","La serie narra las aventuras de Ichigo Kurosaki, un adolescente que tiene la facultad de interactuar con los espíritus. Una noche, Ichigo se " +
-                "encuentra con una shinigami —personificación japonesa del Dios de la muerte— llamada Rukia Kuchiki, quien se sorprende que pueda verla. Sin embargo, su conversación es interrumpida por la aparición de un" +
-                " hollow, un espíritu maligno. Después de que Rukia fuera gravemente herida al tratar de proteger a Ichigo, esta intenta traspasarle parte de sus poderes a Ichigo para que pueda enfrentarse al hollow en igualdad " +
-                "de condiciones","05/10/2004",366,"Pierrot");
+        crearAnime("Toradora","*_*","romance","La historia de Toradora! empieza con el protagonista masculino, Ryūji Takasu, quien está frustrado por tratar de verse bien al entrar a " +
+                "segundo año de preparatoria. A pesar de su personalidad gentil, sus ojos los hacen parecer un delincuente intimidante, de manera que se siente sin posibilidades de encajar, especialmente entre sus " +
+                "compañeros.","26/03/2009",25,"J.C.Staff");
 
-        crearAnime("Shingeki no Kyojin","q.q","fantasía","Hace más de 100 años, la humanidad es casi aniquilada por unas siniestras criaturas desde unos 3 hasta 15 metros de altura,conocidas como los " +
-                "Titanes, que surgen de la nada con un único propósito: devorar seres humanos. No obstante, un grupo reducido de personas pudo sobrevivir dentro de una enorme área de terreno amurallada, protegida por muros de 50 metros" +
-                " de altura, el triple de los titanes más altos registrados hasta la fecha de su construcción","06/04/2013",30,"Wit Studio");
+        crearAnime("D-Frag","@_@","Comedia","Kenji Kazama es un joven delincuente con muy mala fama que, un día se cruza con el llamado “Club de creación de juegos” que preside una chica " +
+                "llamada Ruka Shibasaki (atributo fuego/oscuridad) y al que también asisten la presidenta del consejo estudiantil Chitose Karasuyama (atributo tierra), Sakura Mizukami de primer año (atributo agua) y cuya " +
+                "consejera es la profesora de segundo Minami Osawa (atributo rayo), que acaban “forzándolo” a unirse al club. Ahora Kazama está rodeado de un grupo de gente de lo más rara.","24/03/2014",
+                24,"Brain's Base");
+
+        crearAnime("Tengen Toppa Gurren-Lagann","YwY","Mecha","Gurren Lagann tiene lugar en un futuro donde la Tierra está dominada por el Rey Espiral, Lordgenome, que obliga a la humanidad a vivir" +
+                " en aldeas subterráneas aisladas. Estos pueblos no tienen contacto con el mundo de la superficie u otros pueblos, estando bajo la constante amenaza de terremotos. Los cavadores son aldeanos reclutados para expandir" +
+                " las casas subterráneas cada vez a mayor profundidad","29/04/2007",27,"Gainax");
+
+        crearAnime("Code Geass","uwu","Mecha","El Sacro Imperio de Britannia declaró la guerra a Japon. Sobrepasó a las fuerzas japonesas y conquistó el país en menos de un mes, haciendo uso de sus nuevas armas" +
+                " robóticas, los Knightmare Frames. Japón perdió su libertad y derechos, y la ahora colonia pasó a ser llamado \"Área 11\". Sus habitantes, ahora llamados \"Elevens\" (En español: Onces), fueron obligados a sobrevivir en" +
+                " guetos.","28/07/2007",25,"Sunrise");
+
+        crearAnime("Detective Conan",":v","misterio","El detective juvenil más famoso de Japón, el estudiante de secundaria, él es Shinichi Kudo, se encuentra en un parque de atracciones junto a su amiga de la " +
+                "infancia Ran Mouri, de la que está secretamente enamorado pero él no sabe que ella también lo está de él. Allí, después de resolver un nuevo caso de muerte, se separa de su amiga y presencia un intercambio sospechoso de dinero" +
+                " entre uno de los hombres vestidos de negro que estaba presente en el caso anterior y un hombre misterioso. Mientras los espiaba, Shinichi es atacado por la espalda por el compañero del hombre de negro, y le administran un veneno " +
+                "el APTX 4869 con el fin de matarlo. Sin embargo el veneno tuvo un efecto inesperado: el cuerpo de Shinichi pasó a convertirse en el de un niño de 7 años.","08/01/1996",900,"TMS Entertainment");
+
+        crearAnime("Sword Art Online","$:","romance","Escapar es imposible hasta terminar el juego; un game over significaría una verdadera \"muerte\". Sin saber la \"verdad\" de la siguiente generación del Multijugador " +
+                "Masivo Online, 'Sword Art Online(SAO)', con 10 mil usuarios unidos juntos abriendo las cortinas para esta cruel batalla a muerte. Participando solo en SAO, el protagonista Kirito ha aceptado inmediatamente la \"verdad\" de este MMO." +
+                " Y, en el mundo del juego, un gigante castillo flotante llamado 'Aincrad', Kirito se distinguió a si mismo como un jugador solitario. Apuntando a terminar el juego al alcanzar la planta mas alta el solo continua avanzando arriesgadamente " +
+                "hasta que recibe una invitación a la fuerza de una guerrera y esgrimista experta, Asuna, con la cual tendra que hacer equipo.","07/07/2012",25,"A-1 Pictures");
+
+        crearAnime("Death Note","=_=","misterio","La historia es protagonizada por Light Yagami, un estudiante sobresaliente de Japón que tiene una perspectiva «aburrida» de la vida y quien considera el mundo como un lugar «podrido»." +
+                " Un día, su vida sufre un cambio radical, cuando encuentra un extraño cuaderno sobrenatural llamado «Death Note», tendido en el suelo. Detrás de la portada de dicho cuaderno había instrucciones sobre su uso, donde decía que si se escribía el nombre de " +
+                "una persona y se visualizaba mentalmente el rostro de ésta, moriría de un ataque al corazón. Al principio, Light desconfiaba de la autenticidad del cuaderno, pero luego de probarlo en dos ocasiones, se da cuenta que su poder era " +
+                "real","26/06/2007",37,"Madhouse");
 
 
     }
