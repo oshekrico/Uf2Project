38ea21ee34364b34ebc6bb060d2bfeb5bf578939
sirus98 <clopez@elpuig.xeill.net>
2018-04-23 20:11:41 +0200

    Control de errores en busqueda de animes y añadido nuevos animus

--- src/com/company/view/misanimes/PantallaMisAnimes.java
+++ src/com/company/view/misanimes/PantallaMisAnimes.java
@@ -7,13 +7,15 @@ public class PantallaMisAnimes {
     public void show(ManagerUsuarios managerUsuarios, ManagerAnimes managerAnimes){
         System.out.println("\033[104;97mAnimeApp:: MisAnimes\033[0m");
         System.out.println(" ");
-        System.out.println("Estos son tus Animes: ");
+        System.out.println("Estos son MisAnimes: ");
+        System.out.println(" ");
 
         for (int i = 0; i < managerUsuarios.logeado.misAnimes.length; i++) {
             if(managerUsuarios.logeado.misAnimes[i] != null) {
                 System.out.println(managerUsuarios.logeado.misAnimes[i].titulo);
             }
         }
+        System.out.println(" ");
 
     }
 }
